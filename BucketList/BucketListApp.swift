196614bffd03b0f49781b2dde36a056753dc6ee4
Mberndt92 <maximilian_berndt@icloud.com>
2023-04-12 07:09:57 +0900

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Maximilian Berndt on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
