49d8989fd5d84cabba5b279888be10909f350eff
Mberndt92 <maximilian_berndt@icloud.com>
2023-04-15 11:20:10 +0900

    Finish Day 72

---
+++ BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Maximilian Berndt on 2023/04/15.
+//
+
+import Foundation
+import LocalAuthentication
+import MapKit
+
+extension ContentView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published var mapRegion = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 50,longitude: 0),
+            span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25)
+        )
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
+        
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+        
+        func addLocation() {
+            let newLocation = Location(
+                id: UUID(),
+                name: "New Location",
+                description: "",
+                latitude: mapRegion.center.latitude,
+                longitude: mapRegion.center.longitude
+            )
+            
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location) {
+            guard
+                let selectedPlace = selectedPlace,
+                let index = locations.firstIndex(of: selectedPlace) else { return }
+            locations[index] = location
+            save()
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reasonString = "Please authenticate yourself to unlock your places."
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reasonString) { success, authenticationError in
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // error
+                    }
+                }
+            } else {
+                // no biometrics
+            }
+            
+        }
+    }
+}
--- BucketList/Filemanager+DocumentsDirectory.swift
+++ BucketList/Filemanager+DocumentsDirectory.swift
@@ -9,19 +9,22 @@ import Foundation
 
 extension FileManager {
     
+    static var documentsDirectory: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
     func saveInDocuments<T: Encodable>(to filename: String, data: T) {
-        let url = getDocumentsDirectoryURL().appendingPathComponent(filename)
+        let url = FileManager.documentsDirectory.appendingPathComponent(filename)
         do {
             let encodedData = try JSONEncoder().encode(data)
             try encodedData.write(to: url, options: .atomic)
         } catch {
             fatalError("Failed to save to documents directory")
         }
-        
     }
     
     func loadFromDocuments<T: Decodable>(_ fileName: String) -> T {
-        let url = getDocumentsDirectoryURL().appendingPathComponent(fileName)
+        let url = FileManager.documentsDirectory.appendingPathComponent(fileName)
         do {
             let data = try Data(contentsOf: url)
             let decodedData = try JSONDecoder().decode(T.self, from: data)
@@ -30,9 +33,5 @@ extension FileManager {
         } catch {
             fatalError("Failed to load data from documents directory")
         }
-    }
-    
-    private func getDocumentsDirectoryURL() -> URL {
-        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
     }
 }
